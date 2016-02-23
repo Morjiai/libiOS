21f60409b278ab058db20d5207764ab5cc699e5f
Morjiai <Morjiai@gmail.com>
2016-02-22 19:53:23 -0500

    install libimobiledevice
    Linux only

---
+++ libs.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+echo "Installing libs"
+read -r -p "Are you using ubuntu? [y/N] " response
+case $response in
+    [yY][eE][sS]|[yY]) 
+        libusbmuxd-dev libplist-dev  libplist++-dev libgnutls-dev libssl-dev usbmuxd make automake autoconf libtool pkg-config gcc cython doxygen
+        mkdir -p ~/src
+cd ~/src
+git clone https://github.com/libimobiledevice/libimobiledevice.git
+cd libimobiledevice
+./autogen.sh
+make
+        ;;
+    *)
+        echo "Sorry not supported!";
+        ;;
+esac
+
