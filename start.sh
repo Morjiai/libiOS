e40301bdfa9927455a15f514176eacc7770ea448
Morjiai <Morjiai@gmail.com>
2016-02-27 11:23:08 -0500

    Tested and working.

--- start.sh
+++ start.sh
@@ -7,9 +7,12 @@ echo "4.) Run outdated SSH legacy exploit."
 
 read n
 case $n in
-    1.) clang -o libiOS/9.3/B3/s3tz7.m;;
-    2.) sh libs.sh;;
-    3.) sh extras.sh;;
-    4.) python SSH/legacy.py;;
-    *) invalid option;;
+    1) clang -o 9.3/B3/s3tz7.m
+;;
+    2) sh Linux/libs.sh
+;;
+    3) sh Linux/extras.sh
+;;
+    4) python SSH/legacy.py
+;;
 esac
