e6d65348726dbf9e5ed1ef6e53a8345e8246d54a
Morjiai <Morjiai@gmail.com>
2016-02-27 10:46:02 -0500

    Should work now.
    Menu outline by: http://askubuntu.com/users/109540/harlemsquirrel

--- start.sh
+++ start.sh
@@ -1,20 +1,12 @@
 #!bin/bash
 echo "Getting ready to set up libiOS"
-read -r -p "Would you like to build 9.3/b3/s3tz7.m? [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY]) 
-        clang -o 9.3/b3/s3tz7.m
-        ./a.out
-        ;;
-    *)
-    read -r -p "Would you like to run legacy SSH exploit?[y/N] " response
-case $response in
-    [yY][eE][sS]|[yY]) 
-        python SSH/legacy.py
-        ;;
-    *)
-        echo !";
-        ;;
-        ;;
-esac
+read -n 1 -p "Would you like to exit, build 9.3/b3/s3tz7.m or run legacy SSH exploit ? (E/b/r) " ans;
 
+case $ans in
+    r|R)
+       python SSH/legacy.py;;
+    b|B)
+       clang -o 9.3/b3/s3tz7.m;;
+    *)
+        exit ;;
+esac
