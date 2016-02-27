6b530d5d510f83135ecce8b6f9165ed0f389650b
Morjiai <Morjiai@gmail.com>
2016-02-27 10:47:49 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 echo "Getting ready to set up libiOS"
-read -n 1 -p "Would you like to exit, build 9.3/b3/s3tz7.m or run legacy SSH exploit ? (E/b/r) " ans;
+read -r 1 -b "Would you like to exit, build 9.3/b3/s3tz7.m or run legacy SSH exploit ? (E/b/r) " ans;
 
 case $ans in
     r|R)
