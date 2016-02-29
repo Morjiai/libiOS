3d1d8efc5903a4736880db4bb9a2166a709ff09e
Morjiai <Morjiai@gmail.com>
2016-02-29 16:38:55 -0500

    Fixed install script.

--- Linux/libs.sh
+++ Linux/libs.sh
@@ -3,7 +3,7 @@ echo "Installing libs"
 read -r -p "Are you using ubuntu? [y/N] " response
 case $response in
     [yY][eE][sS]|[yY]) 
-        libusbmuxd-dev libplist-dev  libplist++-dev libgnutls-dev libssl-dev usbmuxd make automake autoconf libtool pkg-config gcc cython doxygen
+        sudo apt-get install libusbmuxd-dev libplist-dev  libplist++-dev libgnutls-dev libssl-dev usbmuxd make automake autoconf libtool pkg-config gcc cython doxygen
         mkdir -p ~/src
 cd ~/src
 git clone https://github.com/libimobiledevice/libimobiledevice.git
