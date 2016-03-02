9da61baaada57e161af39bfd304fedfa02196e13
Morjiai <Morjiai@gmail.com>
2016-03-02 10:02:12 -0500

    Unmounts HFS all credits to BlackGeekTutorial

---
+++ Tools/unmount-hfs.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo "Getting files."
+wget https://raw.githubusercontent.com/BlackGeekTutorial/hfs-mounter-for-ios/master/usr/bin/hfsunmounter.sh
+echo "All credit goes to BlackGeekTutorial"
+sh hfsunmounter.sh
