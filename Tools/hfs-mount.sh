671bc8d2d06c7f22e4d85d5af098af3b50acaeab
Morjiai <Morjiai@gmail.com>
2016-03-02 10:00:09 -0500

    Mounts HFS all credits to BlackGeekTutorial

---
+++ Tools/hfs-mount.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo "Downloading..." 
+wget https://raw.githubusercontent.com/BlackGeekTutorial/hfs-mounter-for-ios/master/usr/bin/hfsmounter.sh
+echo "Tool created by BlackGeekTutorial."
+sh hfsmounter.sh
