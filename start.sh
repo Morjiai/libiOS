50d188d65dc8233db34262fafbf2804db8bf39c1
Morjiai <Morjiai@gmail.com>
2016-03-02 10:55:35 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 echo "Welcome to libiOS!"
-echo "1.) Compile  libiOS/9.3/B3/s3tz7.m"
-echo "2.) Compile linux library."
-echo "3.) Compile Extras."
-echo "4.) Run outdated SSH legacy exploit." 
-echo "5.) Mount HFS." 
+echo "1.) Compile linux library."
+echo "2.) Compile Extras."
+echo "3.) Run outdated SSH legacy exploit." 
+echo "4.) Mount HFS." 
 
 read n
 case $n in
-    1) clang -o 9.3/B3/s3tz7.m
+    1) sh Linux/libs.sh
 ;;
-    2) sh Linux/libs.sh
+    2) sh Linux/extras.sh
 ;;
-    3) sh Linux/extras.sh
+    3) python SSH/legacy.py
 ;;
-    4) python SSH/legacy.py
-;;
-    5) sh Tools/hfs-mount.sh
+    4) sh Tools/hfs-mount.sh
 ;;
 esac
